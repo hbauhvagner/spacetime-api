462fed2588db2dd5c7d9426dc534b50c1f9ec5e7
hbauhvagner <hbauhvagner@gmail.com>
2023-09-07 19:14:42 -0300

    Projeto finalizado

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import { memoriesRoute } from './routes/memories'
+
+const app = fastify()
+
+app.register(cors, {
+  origin: true,
+})
+app.register(memoriesRoute)
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running on http://localhost:3333')
+  })
